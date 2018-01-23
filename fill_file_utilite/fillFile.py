c3b384241627e252e8512574cfc4f9ebe9bcfb4d
Denis_N <nebyt@mail.ru>
2018-01-24 00:49:28 +0300

    repaired utility

--- fill_file_utilite/fillFile.py
+++ fill_file_utilite/fillFile.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import time
 import sys
 import threading
@@ -24,11 +27,12 @@ th = threading.Thread(target=press_key,name=2)
 
 def fill_file():
     k = 1
-    words: list
-    with open('dict.txt', 'r') as dictionary:
+    words = []
+    crt_drt = os.path.dirname(os.path.realpath(__file__))
+    with open('{0}{1}dict.txt'.format(crt_drt, os.sep), 'r') as dictionary:
         words = dictionary.read().split('\n')
     for count in range(11):
-        file_path = '{0}{1}{2}'.format(os.getcwd(), os.sep, 'test_{0}.log'.format(count))
+        file_path = '{0}{1}{2}'.format(crt_drt, os.sep, 'test_{0}.log'.format(count))
         if not os.path.exists(r'{0}'.format(file_path)):
             while True:
                 if pressed_key == 'q':
@@ -37,11 +41,11 @@ def fill_file():
                 else:
                     w1 = random.choice(words)
                     w2 = random.choice(words)
-                    with open('test_{0}.log'.format(count), 'a') as file:
+                    with open('{0}{1}test_{2}.log'.format(crt_drt, os.sep, count), 'a') as file:
                         file.writelines('New string {0} {1} Новая строка {0} {2}\n'.format(k, w1, w2))
                     print('New string {0} {1} Новая строка {0} {2}\n'.format(k, w1, w2))
                     k += 1
-                time.sleep(1)
+                time.sleep(0.5)
         else:
             pass
 
